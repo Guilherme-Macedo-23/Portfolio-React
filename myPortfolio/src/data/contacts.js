079d691596d69f4ba47b2e706bc2cb0e2f3cbf84
Guilherme Macedo <guilhermemacedo2323@gmail.com>
2022-06-01 13:55:43 -0300

    Add files via upload

---
+++ myPortfolio/src/data/contacts.js
@@ -0,0 +1,7 @@
+const contacts = [
+  {social: 'email', href: 'mailto:guilhermemacedo.fullstack@gmail.com', text:'guilhermemacedo.fullstack@gmail.com'},
+  {social: 'linkedin', href: 'https://www.linkedin.com/in/guilherme-macedo-19b542208/', text: "in/guilherme-macedo"},
+  {social: 'github', href: 'https://github.com/Guilherme-Macedo-23', text: "/guilherme-macedo"}
+];
+
+export default contacts;
